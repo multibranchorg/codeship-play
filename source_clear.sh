cfa930c995e706c43703d61b5f4c747a775c141b
Michael Neale <michael.neale@gmail.com>
2018-03-05 17:43:08 +1100

    try this with suppor thelp

--- source_clear.sh
+++ source_clear.sh
@@ -1,1 +1,8 @@
-curl -sSL https://download.sourceclear.com/ci.sh | bash
+export SCAN_DIR="target/"; 
+export SRCCLR_SCM_NAME=https://github.com/multibranchorg/$CI_REPO_NAME; 
+export SRCCLR_SCM_URI=https://github.com/multibranchorg/$CI_REPO_NAME; 
+export SRCCLR_SCM_REF=$CI_BRANCH; 
+export SRCCLR_SCM_REV=$CI_COMMIT_ID; 
+curl -sSL https://download.sourceclear.com/ci.sh | sh
+
+
