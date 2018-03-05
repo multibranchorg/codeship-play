1afd5583adc70c3637d036abdee779877eac8b68
Michael Neale <michael.neale@gmail.com>
2018-03-05 19:53:38 +1100

    trying this other thing

--- source_clear.sh
+++ source_clear.sh
@@ -1,5 +1,7 @@
-#export SCAN_DIR="target/"; 
-export SCAN_DIR="/"
+export SCAN_DIR="target/"; 
+cp ./pom.xml target/
+cp -r src target/
+#export SCAN_DIR="/"
 export SRCCLR_SCM_NAME=https://github.com/multibranchorg/$CI_REPO_NAME; 
 export SRCCLR_SCM_URI=https://github.com/multibranchorg/$CI_REPO_NAME; 
 export SRCCLR_SCM_REF=$CI_BRANCH; 
